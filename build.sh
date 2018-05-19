9b8beda795a783055bdad06db529694d232aa479
georgezeng <zzeng@bglcorp.com.au>
2018-05-19 12:53:44 +0800

    no message

--- build.sh
+++ build.sh
@@ -1,8 +1,9 @@
 mkdir ~/app
+cd ~/app
 if [ ! -d "pdf-ca" ]; then
   sudo docker run --rm -it -v ~/app:/git alpine/git clone https://github.com/georgezeng/pdf-ca.git
 else
-  cd ~/app/pdf-ca && sudo docker run --rm -it -v ~/app/pdf-ca:/git alpine/git pull
+  sudo docker run --rm -it -v ~/app/pdf-ca:/git alpine/git pull
 fi
 sudo docker run --rm -it -v ~/.m2:/root/.m2 -v ~/app/pdf-ca:/usr/src/maven -w /usr/src/maven maven mvn clean verify
 cd ~/app/pdf-ca
